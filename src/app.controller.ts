9d8cc525b2b329ee4d8c3b62409dfc0692c75ead
raviraj-kher <ravirakher462@gmail.com>
2022-04-06 18:14:20 +0530

    First Program using NestJs

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Put, Delete,Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    // return this.appService.getHello();
+    return "Hi Welcome to first program of NestJs";
+  }
+
+  @Get(':name')
+  getHelloWordName(@Param() payload): string {
+    // return this.appService.getHello();
+    console.log(payload);
+    return "My name is " + payload.name;
+  }
+}
