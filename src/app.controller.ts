2a4d6c065b8934eda0e60d1d93f6fa6bbf045669
raviraj-kher <ravirajkher462@gmail.com>
2022-04-13 22:16:06 +0530

    Simple loginFunctionality using passport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Put, Delete,Param } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-    // return "Hi Welcome to first program of NestJs";
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
-
+  // constructor(private readonly appService: AppService) {}
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  //   // return "Hi Welcome to first program of NestJs";
+  // }
   // @Get(':name')
   // getHelloWordName(@Param() payload): string {
   //   // return this.appService.getHello();
